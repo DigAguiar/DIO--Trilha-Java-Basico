64de2f5681ca2873a30ca5a0f00b90aa903fb023
Diogo de Aguiar Barreto <87386717+DigAguiar@users.noreply.github.com>
2024-04-25 10:50:58 -0300

    Add files via upload

---
+++ ContaTerminal.java
@@ -0,0 +1,31 @@
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+public class ContaTerminal {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int numeroConta;
+        String agencia;
+        String nomeCliente;
+        float saldo = 230.99f;
+
+        System.out.println("Por favor, digite o número da conta: ");
+        numeroConta = scanner.nextInt();
+
+        scanner.nextLine();
+
+        System.out.println("Por favor, digite a agência: ");
+        agencia = scanner.nextLine();
+
+        System.out.println("Por favor, seu nome: ");
+        nomeCliente = scanner.nextLine();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco! " +
+                "\nO Numero da sua agência: " + agencia + " " +
+                "\nnúmero da sua conta: " + numeroConta + "" +
+                "\ne seu saldo inicial: " + saldo + " reais.");
+
+    }
+}
